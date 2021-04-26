1bc3fa05ea88ebdb96011960adf25b1894ab51e6
Adrian Moennich <adrian@planetcoding.net>
2021-04-26 22:43:15 +0200

    Support running inside the flask dev server

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import uwsgi
 from flask import Flask, render_template, request
 from redis import StrictRedis
 
@@ -13,6 +12,21 @@ def index():
 
 @app.route('/subscribe')
 def subscribe():
+    try:
+        import uwsgi
+    except ImportError:
+        # If we're not running inside uWSGI, we make use of the fact that
+        # the response of a Flask view can also be another WSGI application,
+        # and while not being particularly scalable, we can run the SSE app
+        # within the Flask dev server.
+        import sseapp
+        def _app(environ, start_response):
+            environ['OFFLOAD_USER_IP'] = request.remote_addr
+            return sseapp.application(environ, start_response)
+        return _app
+
+    # If we are running inside uWSGI, ask it to offload the request to the
+    # gevent-based instance which only runs the SSE app.
     uwsgi.add_var('OFFLOAD_TO_SSE', 'y')
     uwsgi.add_var('OFFLOAD_SERVER', '/tmp/uwsgi-offload.sock')
     uwsgi.add_var('OFFLOAD_USER_IP', request.remote_addr)
